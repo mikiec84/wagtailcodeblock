821178aa2913d03577a7ee32a500e8b03e1f0790
Allen, Timothy <tallen@wharton.upenn.edu>
2018-03-18 19:26:06 +0000

    WIP: code to render the blocks once.

--- wagtailcodeblock/templatetags/wagtailcodeblock_tags.py
+++ wagtailcodeblock/templatetags/wagtailcodeblock_tags.py
@@ -1,4 +1,4 @@
-from django.template import Library
+from django.template import Library, Node
 from django.utils.safestring import mark_safe
 
 from ..settings import (
@@ -36,3 +36,45 @@ def load_prism_js():
         prism_version,
     )
     return mark_safe(script)
+
+
+@register.tag
+def renderonce(parser, token):
+    """
+    Usage: {% renderonce %}This will only be rendered the first time.{% endrenderonce %}
+    This is useful in StreamField blocks for JS and CSS.
+    """
+
+    nodelist = parser.parse(('endrenderonce',))
+    parser.delete_first_token()
+
+    return RenderOnceNode(nodelist)
+
+
+class RenderOnceNode(Node):
+    """
+    Class to store which sections have already been rendered once, so they aren't
+    rendered again. This is useful in a StreamField that needs CSS or JS included
+    once, but not in each section.
+    """
+
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context: dict) -> str:
+        from hashlib import md5
+
+        context['already_rendered'] = context.get('already_rendered', set())
+
+        output = self.nodelist.render(context)
+        output_hash = md5(output.encode('utf-8')).hexdigest()
+
+        print(output_hash)
+        print(context['already_rendered'])
+
+        if output_hash in context['already_rendered']:
+            return ''
+
+        context['already_rendered'].add(output_hash)
+
+        return self.nodelist.render(context)
